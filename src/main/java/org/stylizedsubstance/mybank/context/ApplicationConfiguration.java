6249174b3a2fca01bd675fbbcedd5eca5fbf6877
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2026-02-10 10:54:48 +0200

    added thymeleaf dependency and configuration beans

--- src/main/java/org/stylizedsubstance/mybank/context/ApplicationConfiguration.java
+++ src/main/java/org/stylizedsubstance/mybank/context/ApplicationConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.stylizedsubstance.mybank.ApplicationLauncher;
 import org.stylizedsubstance.mybank.util.DateTimeUtil;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
@@ -22,5 +25,30 @@ public class ApplicationConfiguration {
     @Bean
     public DateTimeUtil dateTimeUtil() {
         return new DateTimeUtil();
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+
+        viewResolver.setOrder(1); // optional
+        viewResolver.setViewNames(new String[] {"*.html", "*.xhtml"}); // optional
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setCacheable(false);
+        return templateResolver;
     }
 }
